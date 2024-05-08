bcfb10664cdc99429cfaed10a36800b5910604c1
meloniouss <firehead765@gmail.com>
2024-05-08 03:22:51 -0400

    added config.txt for on-the-fly configuraton, added python-style comments in the config.txt, refactored code and made it more efficient

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
 import os
 import shutil
 
-path = "C:/Users/fireh/Downloads/"
+extension_destinations = {}
+path = ''
+with open("config.txt", "r") as cfgFile:
+
+    for line in cfgFile:  # looking for the path in this loop
+        line = line.strip()
+        if not line.startswith("#") and "=" in line:
+            path = line.split("=")[1]
+            break
+    for line in cfgFile:
+        line = line.strip()
+        if line.startswith("#"): # here we check whether the line is a comment or not
+            continue
+        else:
+            extensions, folder = line.strip().split(":")
+            for extension in extensions.split(","):
+                extension_destinations[extension.strip()] = folder.strip()
+
+# prompt the user to enter a file path if it's empty
+if path == '':
+    path = input(
+        "Please enter a file path in the following format: Drive:/Folder/File (...), then press enter to select the "
+        "folder you would like to sort.")
+    # Write the file path to config.txt
+    with open("config.txt", "w") as pathFile:
+        pathFile.write("SORTING_PATH=" + path)
+
+# creating destination directories if they don't exist
+for destination in set(extension_destinations.values()):
+    destination_dir = os.path.join(path, destination)
+    if not os.path.exists(destination_dir):
+        os.makedirs(destination_dir)
+
 files = os.listdir(path)
 for file in files:
-    if ".pdf" in file and not os.path.exists(path + "PDFs/" + file):
-        shutil.move(path + file, path + "PDFs/" + file)
-    elif ".jpg" in file and not os.path.exists(path + "Images/" + file):
-        shutil.move(path + file, path + "Images/" + file)
-    elif ".png" in file and not os.path.exists(path + "Images/" + file):
-        shutil.move(path + file, path + "Images/" + file)
-    elif ".docx" in file and not os.path.exists(path + "Docs/" + file):
-        shutil.move(path + file, path + "Docs/" + file)
-    elif ".txt" in file and not os.path.exists(path + "Docs/" + file):
-        shutil.move(path + file, path + "Docs/" + file)
-    elif ".csv" in file and not os.path.exists(path + "Excels/" + file):
-        shutil.move(path + file, path + "Excels/" + file)
-    elif ".exe" in file and not os.path.exists(path + "Executables/" + file):
-        shutil.move(path + file, path + "Executables/" + file)
-    elif ".mov" in file and not os.path.exists(path + "Videos/" + file):
-        shutil.move(path+file, path + "Videos/" + file)
-    elif ".mp4" in file and not os.path.exists(path + "Videos/" + file):
-        shutil.move(path+file, path + "Videos/" + file)
-    elif ".wmv" in file and not os.path.exists(path + "Videos/" + file):
-        shutil.move(path+file, path + "Videos/" + file)
-    elif ".avi" in file and not os.path.exists(path + "Videos/" + file):
-        shutil.move(path+file, path + "Videos/" + file)
-    elif ".zip" in file and not os.path.exists(path + "Zips/" + file):
-        shutil.move(path+file, path + "Zips/" + file)
-    elif ".rar" in file and not os.path.exists(path + "Zips/" + file):
-        shutil.move(path + file, path + "Zips/" + file)
-    elif ".webp" in file and not os.path.exists(path + "random webpages i downloaded on accident/" + file):
-        shutil.move(path + file, path + "random webpages i downloaded on accident/" + file)
-    elif ".html" in file and not os.path.exists(path + "random webpages i downloaded on accident/" + file):
-        shutil.move(path + file, path + "random webpages i downloaded on accident/" + file)
+    file_extension = os.path.splitext(file)[1]
+    if file_extension in extension_destinations:
+        destination_dir = os.path.join(path, extension_destinations[file_extension])
+        destination_file_path = os.path.join(destination_dir, file)
+        if not os.path.exists(destination_file_path):
+            shutil.move(os.path.join(path, file), destination_file_path)
